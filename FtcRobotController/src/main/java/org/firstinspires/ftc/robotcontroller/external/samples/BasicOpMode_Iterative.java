5a32c2222dc51f3b654a775ba6dbfbd103d18973
Poggers69-PNG <72159679+Poggers69-PNG@users.noreply.github.com>
2021-11-27 11:07:49 -0700

    Code for arm cascade thingy

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 @Disabled
 public class BasicOpMode_Iterative extends OpMode
@@ -58,6 +58,8 @@ public class BasicOpMode_Iterative extends OpMode
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
+    private DcMotor armDrive = null;
+
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -66,16 +68,18 @@ public class BasicOpMode_Iterative extends OpMode
     public void init() {
         telemetry.addData("Status", "Initialized");
 
+        armDrive = hardwareMap.dcMotor.get("arm"); //definition for armDrive motor
+        armDrive = hardwareMap.get(DcMotor.class, "armDrive");
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        //leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        //rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        //leftDrive.setDirection(DcMotor.Direction.FORWARD);
+       //rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -86,6 +90,7 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void init_loop() {
+
     }
 
     /*
@@ -115,11 +120,17 @@ public class BasicOpMode_Iterative extends OpMode
         leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
         rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
+        //Code for moving cascade w/ motors
+        double cascade = -gamepad1.left_stick_y;
+        armDrive.setPower(cascade);
+        armDrive.setPower(leftPower);
+        
+
+
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
         // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
+        // rightPower = -gamepad1.right_stick_y
         // Send calculated power to wheels
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
